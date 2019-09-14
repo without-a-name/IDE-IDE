79bd46e598454c9c7490d76557d2af4aec336419
BaiRensan <48916419+BaiRensan@users.noreply.github.com>
2019-09-15 02:08:18 +0800

    Add files via upload
    关键字高亮、关键字补全、行号、当前行高亮

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -6,36 +6,10 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
-    //on_action_find_triggerded();
+    ui->
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
-}
-
-void MainWindow::on_pushButton_clicked()
-{
-    connect(ui->pushButton,SIGNAL(clicked()),this,SLOT(on_action_find_triggerded()));
-}
-void MainWindow::on_action_find_triggerded()
-{
-    QDialog *findDlg = new QDialog(this);
-
-    findDlg->setWindowTitle(tr("查找"));
-    find_textLineEdit = new QLineEdit(findDlg);
-    QPushButton *find_Btn = new QPushButton(tr("查找下一个"),findDlg);
-    QVBoxLayout* layout = new QVBoxLayout(findDlg);
-    layout->addWidget(find_textLineEdit);
-    layout->addWidget((find_Btn));
-    findDlg->show();
-    connect(find_Btn,SIGNAL(clicked()),this,SLOT(show_findText()));
-}
-void MainWindow::show_findText()
-{
-    QString findText = find_textLineEdit->text();
-    if(! ui->textEdit->find(findText,QTextDocument::FindBackward)){
-        QMessageBox::warning(this,tr("查找失败"),tr("找不到该内容"));
-    }
-    ui->plainTextEdit->find()
 }
